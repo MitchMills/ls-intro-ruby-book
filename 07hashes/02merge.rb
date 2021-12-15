7a9efd4f7260cc4392c98016ed8b54bc8423be86
Mitch Mills <mitchmillsgcp@gmail.com>
2021-12-15 17:49:20 -0600

    Hashes exercises 1 and 2

---
+++ 07hashes/02merge.rb
@@ -0,0 +1,13 @@
+hash1 = { key1: "value1", key2: "value2", key3: "value3" }
+hash2 = { key4: 4, key5: 5 }
+
+puts "This is hash 1: " + hash1.to_s
+puts "This is hash 2: " + hash2.to_s
+puts
+puts "This is the result of calling the merge method on them: " + hash1.merge(hash2).to_s
+puts "Hash 1 remains the same: " + hash1.to_s
+puts "Hash 2 remains the same: " + hash2.to_s
+puts
+puts "This is the result of calling the merge! method on them: " + hash1.merge!(hash2).to_s
+puts "Hash 1 has changed: " + hash1.to_s
+puts "Hash 2 remains the same: " + hash2.to_s
