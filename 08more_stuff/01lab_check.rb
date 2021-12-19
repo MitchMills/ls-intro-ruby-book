b26385ce7b521a5325371c847282dc48833171f1
Mitch Mills <mitchmillsgcp@gmail.com>
2021-12-19 11:55:56 -0600

    More Stuff exercise

---
+++ 08more_stuff/01lab_check.rb
@@ -0,0 +1,11 @@
+def lab_check(string)
+  if /lab/i.match(string)
+    puts string + ": match!"
+  else
+    puts string + ": no match."
+  end
+end
+
+words = ["laboratory", "experiment", "Pans Labyrinth", "elaborate", "polar bear"]
+
+words.each {|word| lab_check(word)}
