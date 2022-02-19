abfcc2ee3f46456d1a4561d0c9939fae59f9c340
Mitch Mills <mitchmillsgcp@gmail.com>
2022-02-19 10:33:53 -0600

    Reviewed exercises

--- 01the_basics/exercise01.rb
+++ 01the_basics/exercise01.rb
@@ -1,7 +1,7 @@
 # 1. Add two strings together that, when concatenated, return your first and last name as your full name in one string.
 
-print "First Name:"
+print "First Name: "
 first = gets.chomp
-print "Last Name:"
+print "Last Name: "
 last = gets.chomp
 puts "Your full name is " + first + " " + last
